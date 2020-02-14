c51d8355c334864ea6de2030330485a32c76e196
David J. Copenhaver <davidjcopenhaver@gmail.com>
2020-02-14 14:03:46 -0500

    completed multiline value support

--- ctable.go
+++ ctable.go
@@ -1,5 +1,16 @@
 package ctable
 
+/*
+Purpose: To create nice columnar output (tables) for console apps.
+Author: David J. Copenhaver
+
+Notable features:
+- Automatic column sizing based on the actual data
+- Control over truncation
+- Control over justification
+- Support for mulitline values
+*/
+
 import (
 	"fmt"
 	"log"
@@ -48,27 +59,38 @@ func NewTable(columns []Column) Table {
 	}
 }
 
-// New version of AddRow() to support multiline values in fields - original/previous version commented out below this one
+/*
+	New version of AddRow() to support multiline values in fields - original/previous version commented out below this one
+
+	Example calls:
+	ct.AddRow("string data", "string data", "string data")
+	ct.AddRow("string data", []string{"one", "two", "three", "four"}, "string data")
+
+	Its variadic, so any number of values in any mix of strings and string slices can be used (number of args has to match number of columns of course)
+*/
 func (ct *Table) AddRow(fields ...interface{}) {
 
 	if len(fields) != ct.ColumnCount {
 		log.Fatal("CONSOLETABLE: Cannot add a row of data with more, or fewer, fields than defined columns.")
 	}
 
-	// update max length values and truncation status stored with column defs.
-	// whether truncation *will* be required is stored with the column def so it can be used in display logic,
-	// but we only truncate upon display so we keep all the data
-	// note: max length is initialized to the column name's length when new column object is instantiated,
-	// as it is essentially a part of the data set when it comes to display logic
+	/*
+		Update max length values and truncation status stored with column defs.
+		Whether truncation *will* be required is stored with the column def so it can be used in display logic,
+		but we only truncate upon display so we keep all the data.
+
+		Note: max length is initialized to the column name's length when new column object is instantiated,
+		as it is essentially a part of the data set when it comes to display logic.
+	*/
 
 	rowState := struct {
-		hasMultilineValue     bool
-		multilineFieldIndexes []int
-		currentItemIndexes    map[int]int
+		hasMultilineValue bool
+		mlFieldIndexes    []int       // multiline field indexes (the field indexes that contain a list of values instead of just one, a slice)
+		mlTracker         map[int]int // multiline tracker (need to track which list item to display next for each multiline field)
 	}{}
 
 	rowState.hasMultilineValue = false
-	rowState.currentItemIndexes = make(map[int]int)
+	rowState.mlTracker = make(map[int]int)
 
 	for i := 0; i < ct.ColumnCount; i++ {
 
@@ -86,8 +108,8 @@ func (ct *Table) AddRow(fields ...interface{}) {
 		case []string:
 
 			rowState.hasMultilineValue = true
-			rowState.multilineFieldIndexes = append(rowState.multilineFieldIndexes, i)
-			rowState.currentItemIndexes[i] = 1 // starting 1 instead of zero because first row will be added normally, then multiline values after
+			rowState.mlFieldIndexes = append(rowState.mlFieldIndexes, i)
+			rowState.mlTracker[i] = 0
 
 			for _, str := range v {
 
@@ -104,20 +126,56 @@ func (ct *Table) AddRow(fields ...interface{}) {
 		}
 	}
 
-	//	for i := 0; i < ct.ColumnCount; i++ {
-	//		if ct.Columns[i].maxLength < utf8.RuneCountInString(fields[i].(string)) {
-	//			ct.Columns[i].maxLength = utf8.RuneCountInString(fields[i].(string))
-	//		}
-	//		if ct.Columns[i].truncateAt > 0 && ct.Columns[i].maxLength > ct.Columns[i].truncateAt {
-	//			ct.Columns[i].truncationRequired = true
-	//		}
-	//	}
-
 	if !rowState.hasMultilineValue {
 		// add as normal, each field is an interface{} that's value IS a single string
 		ct.Rows = append(ct.Rows, fields)
 	} else {
 		// deal with multiline fields
+
+		// need to know the highest number of mulitline values among all multiline fields in order to control right number of iterations to handle them all
+		longestMulti := 0
+
+		for _, fi := range rowState.mlFieldIndexes {
+			if len(fields[fi].([]string)) > longestMulti {
+				longestMulti = len(fields[fi].([]string))
+			}
+		}
+
+		for x := 0; x < longestMulti; x++ { // in context of the one 'row', this is the 'down' direction due to multiline values
+
+			// contruct slice to be used to append to ct.Rows
+			var tempFields []interface{}
+			for fi := 0; fi < ct.ColumnCount; fi++ { // ... and this is the 'across' direction
+
+				var isMLField bool
+				for _, mfi := range rowState.mlFieldIndexes {
+					if fi == mfi {
+						isMLField = true
+					}
+				}
+
+				// first row special
+				if x == 0 {
+					if !isMLField {
+						tempFields = append(tempFields, fields[fi])
+					} else {
+						tempFields = append(tempFields, fields[fi].([]string)[0])
+						rowState.mlTracker[fi]++
+					}
+				} else {
+					// not first row, the rest (to support multiline in the table output, empty strings have to be inserted into all the other fields that are not multiline, *after* the first line)
+					if !isMLField {
+						tempFields = append(tempFields, "") // blanks in cells/fields next to cells/fields with multilines after first value is displayed with the rest of the non multiline value row
+					} else {
+						tempFields = append(tempFields, fields[fi].([]string)[x])
+						rowState.mlTracker[fi]++ // keeping track of which item in the multiline list has been handled and which is up next
+					}
+				}
+			}
+
+			//  tempFields should have been appropriately built now to add as a 'row' in the context of a row to be displayed across the screen
+			ct.Rows = append(ct.Rows, tempFields)
+		}
 	}
 }
 
@@ -155,7 +213,7 @@ func (ct *Table) Display(showHeaders bool) {
 
 		for i, col := range ct.Columns {
 			// for each field - build row string including padding for columnar output, justification, and any truncation per column defs
-			fieldData := row[i]
+			fieldData := row[i].(string) // to support multiline values, interfaces were used, at this point each field item should be a single string
 
 			// truncate field value?
 			if col.truncationRequired && utf8.RuneCountInString(fieldData) > col.truncateAt {
